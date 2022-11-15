3430e529cf1e4166863244ad21a8e0071202cfab
Alejandro Texis <texis369alejandro@gmail.com>
2022-11-14 18:27:18 -0600

    Semana 4 JS

--- Semana 4 - Aprendiendo JS/Prácticas Lives/funciones.js
+++ Semana 4 - Aprendiendo JS/Prácticas Lives/funciones.js
@@ -2,16 +2,18 @@ const cuadrado = function(x) {
     return x * x;
 }
 let numero = 4;
-console.log(cuadrado(numero));
+let numero2 = 2;
+let sumanumerocuadrados= cuadrado(numero) + cuadrado(numero2);
+console.log(sumanumerocuadrados);
 
-
+///////////////////////////////////////////////
 const ruido = function () {
     console.log("kataplum!");
 }
 
 ruido();
 
-
+//////////////////////////////////////////////////
 const exponencial = function (base, exponente) {
     let resultado = 1;
     for (let i = 0; i < exponente; i++){
@@ -19,31 +21,30 @@ const exponencial = function (base, exponente) {
     }
     return resultado;
 }
+let resultado= exponencial(4,3);
+console.log(resultado);
 
-console.log(exponencial(4,3))
-
+/////////////////////////////////////////////
 
 console.log(sumar(5,65));
 function sumar(x, y) {
     return x + y;
 }
 
+//////////////////////////////////////////////////////
 
 const restar = (a, b) => {
     return a - b;
 }
 console.log(restar(40, 8));
 
-
+/////////////////////////////////////////////////
 function saludar(quien) {
   console.log("Hola " + quien);
-  return;
 }
-saludar("Explorer");
-console.log("Bye");
+saludar("Alejandro");
 
-
-
+/////////////////////////////////////////////////////////////
 //Excepciones
 function preguntaDireccion(pregunta) {
   let result = prompt(pregunta);
@@ -53,7 +54,7 @@ function preguntaDireccion(pregunta) {
 }
 
 function mirar() {
-  if (preguntaDireccion("A que lado?") == "I") {
+  if (preguntaDireccion("A que lado? (Derecha o izquierda)") == "I") {
     return "una casa";
   } else {
     return "2 osos hambrientos";
@@ -61,40 +62,10 @@ function mirar() {
 }
 
 try {
-  console.log("Mira a ", mirar());
+  console.log("Mira!", mirar());
 } catch (error) {
   console.log("Hubo un error: " + error);
 }
 
 
-
-//Asincrono
-setTimeout(() => console.log("Tick"), 500);
-
-let fifteen = Promise.resolve(15);
-fifteen.then(value => console.log(`Got ${value}`));
-
-const promesa = () =>
-  new Promise((resolve, reject) =>
-    setTimeout(
-      () => (resolve(console.log('Todo cool')), reject(new Error('oops'))),
-      2000
-    )
-  )
-
-async function main() {
-  //   promesa()
-  //     .then(() => {
-  //       promesa()
-  //         .then(() => console.log('hola'))
-  //         .catch((err) => console.error(err))
-  //     })
-  //     .catch((err) => console.error(err))
-
-  await promesa();
-  console.log('Aquí termina la primer promesa');
-  await promesa();
-  console.log('Aquí termina la segunda promesa');
-}
-
-main();
+///////////////////////////////////////////////////////
